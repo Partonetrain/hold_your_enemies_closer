36cbd66ccb45a8abb402656d15c17827d578c1aa
Partonetrain <Partonetrain@gmail.com>
2024-10-27 15:19:52 -0500

    basic template stuff

---
+++ common/src/main/java/info/partonetrain/hold_your_enemies_closer/Constants.java
@@ -0,0 +1,11 @@
+package info.partonetrain.hold_your_enemies_closer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "hold_your_enemies_closer";
+	public static final String MOD_NAME = "Hold Your Enemies Closer";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ neoforge/src/main/java/info/partonetrain/hold_your_enemies_closer/HYECModNeoforge.java
@@ -0,0 +1,20 @@
+package info.partonetrain.hold_your_enemies_closer;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class HYECModNeoforge {
+
+    public HYECModNeoforge(IEventBus eventBus) {
+
+        // This method is invoked by the NeoForge mod loader when it is ready
+        // to load your mod. You can access NeoForge and Common code in this
+        // project.
+
+        // Use NeoForge to bootstrap the Common mod.
+        Constants.LOG.info("Hello NeoForge world!");
+        CommonClass.init();
+
+    }
+}
